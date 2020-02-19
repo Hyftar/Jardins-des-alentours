69aaf6a02391e8b3f2ab4e3d384c5b9beb5feeba
Hyftar <landry_simon@pm.me>
2020-02-18 23:43:57 -0500

    Add rails ERD for ERD schema generation

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+if Rails.env.development? && ARGV.include?('erd')
+  Zeitwerk::Loader.eager_load_all
+end
