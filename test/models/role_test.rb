be37a74f5a293d92df3360e97f81835ec38e4bb8
Hyftar <landry_simon@pm.me>
2020-02-18 23:45:07 -0500

    Add users and roles tests

---
+++ test/models/role_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class RoleTest < ActiveSupport::TestCase
+  test "should be able to create a new role and add it to a user" do
+    user = users(:one)
+    role = Role.create(name: 'test_role')
+    user.roles << role
+    assert_equal 'test_role', user.roles.first.name
+  end
+
+  test "should not be able to save role without name" do
+    role = Role.new
+    assert_not role.save
+  end
+
+  test "Jane Doe should have two valid roles" do
+    user = users(:two)
+    assert_equal ['Admin', 'Curator'], user.roles.map(&:name)
+  end
+
+  test "should not be able to save role with name shorter than 3 characters" do
+    role = Role.new
+    role.name = 'aa'
+    assert_not role.save
+  end
+
+  test "should be able to save valid role" do
+    role = Role.new
+    role.name = 'test_role_123'
+    assert role.save
+  end
+end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not create user without first and last name" do
+    user = User.new
+    user.email = 'john@exemple.com'
+    user.password = 'test123'
+    assert_not user.save
+  end
+
+  test "should not create user without email" do
+    user = User.new
+    user.first_name = 'John'
+    user.last_name = 'Doe'
+    user.password = 'test123'
+    assert_not user.save
+  end
+
+  test "should be able to create user with valid info" do
+    user = User.new
+    user.first_name = 'John'
+    user.last_name = 'Doe'
+    user.email = 'john@exemple.com'
+    user.password = 'test123'
+    assert user.save
+  end
+
+  test "should not be able to create user with invalid first name" do
+    user = User.new
+    user.first_name = 'J'
+    user.last_name = 'Doe'
+    user.email = 'john@exemple.com'
+    user.password = 'test123'
+    assert_not user.save
+  end
+
+  test "should not be able to create user with invalid last name" do
+    user = User.new
+    user.first_name = 'John'
+    user.last_name = 'D'
+    user.email = 'john@exemple.com'
+    user.password = 'test123'
+    assert_not user.save
+  end
 end
