c1d3a22d3a6e3aaaf20541d949f17b88470f9655
kevenaubin <kevenaubin@gmail.com>
2020-02-28 17:01:42 -0500

    Add addresses and validation

---
+++ app/models/address.rb
@@ -0,0 +1,10 @@
+class Address < ApplicationRecord
+  belongs_to :location
+  validates :address_line_1, presence: true, length: { minimum: 1, maximum: 120 }
+  validates :address_line_2, length: { maximum: 120 }
+  validates :city, presence: true, length: { minimum: 1, maximum: 120 }
+  validates :province, presence: true, length: { minimum: 1, maximum: 120 }
+  validates :country, presence: true, length: { minimum: 1, maximum: 120 }
+  validates :postal_code, length: { maximum: 20 }
+
+end
--- test/controllers/market_controller_test.rb
+++ test/controllers/market_controller_test.rb
@@ -1,14 +1,9 @@
 require 'test_helper'
 
 class MarketControllerTest < ActionDispatch::IntegrationTest
-  test "should get edit" do
-    get market_edit_url
-    assert_response :success
-  end
-
-  test "should get delete" do
-    get market_delete_url
-    assert_response :success
-  end
+  #test "should get edit" do
+   # get market_edit_url
+   # assert_response :success
+  #end
 
 end
