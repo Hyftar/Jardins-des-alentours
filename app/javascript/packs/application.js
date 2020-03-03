55218e60df46eb5b812c7507b8fb222a834a30f6
Hyftar <landry_simon@pm.me>
2020-03-03 13:12:22 -0500

    Landing page fixup and implementation with other pages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
-require("landing_page/lp")
+require("landing_page")
 require("questions")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
