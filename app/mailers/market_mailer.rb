c9f2260acdae854be437e6997cf549c13d6fe0be
kevenaubin <kevenaubin@gmail.com>
2020-03-06 10:10:28 -0500

    Send separate emails based on locale

--- app/mailers/market_mailer.rb
+++ app/mailers/market_mailer.rb
@@ -2,10 +2,9 @@ class MarketMailer < ApplicationMailer
   def market_disponibility_update_email
     @market = params[:market]
     @market_notifications = params[:market_notifications]
-    @emails = []
     @market_notifications.each do |notification|
-      @emails.push(notification.email)
-    end
-    mail(to: "jardinsdesalentours@gmail.com", bcc: @emails, subject: I18n.t('mailer.subject_update_order'))
+      I18n.with_locale(notification.language) do
+        mail(to: notification.email, subject: I18n.t('mailer.subject_update_order'))
+      end
   end
 end
