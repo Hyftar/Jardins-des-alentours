6cb818895e4100c31b7bc86a721b593255b84345
Hyftar <landry_simon@pm.me>
2020-03-10 21:04:51 -0400

    Roles page draft

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+  def toastr_flash
+    flash.each_with_object([]) do |(type, message), flash_messages|
+      type = type == 'notice' ? 'success' : 'error'
+      text = "<script>toastr.#{type}('#{message}', '', { closeButton: true, progressBar: true, positionClass: 'toast-bottom-right' })</script>"
+      flash_messages << text.html_safe if message
+    end.join("\n").html_safe
+  end
+
   # Updates the score of the specified instances of models
   # with a diff (difference between current score and wanted value)
   def self.update_score_of(instances, diff)
