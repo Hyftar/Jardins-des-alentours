064d17ff6af03b7a1bdbc79215711de81a8a4537
kevenaubin <kevenaubin@gmail.com>
2020-03-25 20:29:55 -0400

    Add basic modal implementation

--- app/javascript/gardens/index.js
+++ app/javascript/gardens/index.js
@@ -129,5 +129,35 @@ $(document).on('turbolinks:load', () => {
       accessToken: 'pk.eyJ1Ijoia2V2ZW44MSIsImEiOiJjazdyNDQ3YXQwYTZzM2twNHJ2M2kxdXFtIn0.Vl_ZHudjkrDMIstvKBn53w'
     }).addTo(gardens_page_map);
   }
+  // Modal script, implemented from w3schools
+  // https://www.w3schools.com/howto/howto_css_modals.asp
 
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  btn.onclick = function() {
+    modal.style.display = "block";
+    modal.style.zIndex = "1000";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 })
+
+
